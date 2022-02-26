0f5f6ac7c0035a5a72534ccd041fc9a87d0b4a3d
Kiyomi1931 <71973734+Kiyomi1931@users.noreply.github.com>
2022-02-26 15:50:13 +0900

    Python2-3 ２回目解答
    正常に動作します。

--- techgym2-3-2.py
+++ techgym2-3-2.py
@@ -1,15 +1,20 @@
 import random
 
+data=[['見','貝'],['土','士'],['眠','眼']]
 level = 1
 
 def start_message():
   print('違う漢字の番号(例:A1)を入力してください')
+
+def view_question():
+  print(data[random.randint(0,2)])
 
 def section_message():
   print('レベル:' + str(level))
 
 def play():
   section_message()
+  view_question()
   choice = input('(例:A1)')
   print('デバッグ:choice = ' + choice)
 
